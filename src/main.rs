6b9af81d7fbe4bd183c8ace3e72d85e808da0270
EnergyCube <vincent.mardirossian@epitech.eu>
2022-12-05 17:35:25 +0100

    feat: added project path arg handling and deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Project path
+    #[arg(short = 'p', long = "path", default_value = ".", help = "Specify project path")]
+    project_path: String,
+}
+
 fn main() {
-    println!("Please welcome SolidHunter!");
+    let args = Args::parse();
+
+    println!();
+    println!("SolidHunter: Fast and efficient Solidity linter");
+    println!("Author[{}] Version[{}] License[{}]", env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_VERSION"), "GNU General Public License v3.0");
+    println!();
+
+    println!("Working with project path: {}", args.project_path);
 }
