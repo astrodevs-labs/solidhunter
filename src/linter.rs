2f5d9e5edf3956f2bd8f649db59a427216476b8f
Tristan Gremont <tristan.gremont@epitech.eu>
2022-12-08 17:32:09 +0100

    feat(core-linter): added files utils

--- src/linter.rs
+++ src/linter.rs
@@ -1,4 +1,5 @@
 use crate::lint_result::LintResult;
+use solc_wrapper::Solc;
 
 pub struct SolidFile {
     pub data : Ast,
@@ -11,7 +12,38 @@ pub struct SolidLinter {
 }
 
 impl SolidLinter {
+    
+    fn file_exists(&self, path : &str) -> bool {
+        for file in &self.files {
+            if file.path == path {
+                return true;
+            }
+        }
+        false
+    }
+    
+    fn update_file_ast(&mut self, path : &str, ast : Ast) {
+        for file in &mut self.files {
+            if file.path == path {
+                file.data = ast;
+            }
+        }
+    }
+    
+    fn add_file(&mut self, path : &str, ast : Ast, content : &str) {
+        let file = SolidFile {
+            data : ast,
+            path : path.to_string(),
+            content : content.to_string(),
+        };
+        self.files.push(file);
+    }
+
     fn parse_file(filepath: String) -> LintResult{
+        
+        let ast = Solc::execute_on_file(filepath.to_str()).unwrap();
+        let res = Solc::parse(ast).unwrap();
+        
         LintResult::new()
     }
     
