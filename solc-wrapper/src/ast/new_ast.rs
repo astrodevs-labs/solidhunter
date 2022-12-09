e9f3c3c12ba975a605eab737c62d67c464e85ac5
Alexandre jublot <alexjublot@gmail.com>
2022-12-09 22:26:18 +0100

    fix(new_ast): made AST structure compile

--- solc-wrapper/src/ast/new_ast.rs
+++ solc-wrapper/src/ast/new_ast.rs
@@ -91,58 +91,40 @@ pub enum AssignmentOperator {
 pub enum BinaryOperator {
     #[serde(rename = "+")]
     Plus,
-
     #[serde(rename = "-")]
     Minus,
-
     #[serde(rename = "*")]
     Star,
-
     #[serde(rename = "/")]
     Slash,
-
     #[serde(rename = "%")]
     Percent,
-
     #[serde(rename = "**")]
     DoubleStar,
-
     #[serde(rename = "&&")]
     DoubleAmpersand,
-
     #[serde(rename = "||")]
     DoublePipe,
-
     #[serde(rename = "!=")]
     ExclamationEqual,
-
     #[serde(rename = "==")]
     DoubleEqual,
-
     #[serde(rename = "<")]
     LessThan,
-
     #[serde(rename = "<=")]
     LessThanOrEqual,
-
     #[serde(rename = ">")]
     GreaterThan,
-
     #[serde(rename = ">=")]
     GreaterThanOrEqual,
-
     #[serde(rename = "^")]
     Caret,
-
     #[serde(rename = "&")]
     Ampersand,
-
     #[serde(rename = "|")]
     Pipe,
-
     #[serde(rename = "<<")]
     LeftShift,
-
     #[serde(rename = ">>")]
     RightShift,
 }
@@ -151,25 +133,18 @@ pub enum BinaryOperator {
 pub enum UnaryOperator {
     #[serde(rename = "!")]
     Exclamation,
-
     #[serde(rename = "-")]
     Minus,
-
     #[serde(rename = "+")]
     Plus,
-
     #[serde(rename = "++")]
     DoublePlus,
-
     #[serde(rename = "--")]
     DoubleMinus,
-
     #[serde(rename = "++")]
     PlusPlus,
-
     #[serde(rename = "--")]
     MinusMinus,
-
     #[serde(rename = "delete")]
     Delete,
 }
@@ -178,10 +153,8 @@ pub enum UnaryOperator {
 pub enum FunctionCallKind {
     #[serde(rename = "functionCall")]
     FunctionCall,
-
     #[serde(rename = "structConstructorCall")]
     StructConstructorCall,
-
     #[serde(rename = "typeConversion")]
     TypeConversion,
 }
@@ -190,16 +163,12 @@ pub enum FunctionCallKind {
 pub enum LiteralKind {
     #[serde(rename = "number")]
     Number,
-
     #[serde(rename = "string")]
     String,
-
     #[serde(rename = "hexString")]
     HexString,
-
     #[serde(rename = "unicodeString")]
     UnicodeString,
-
     #[serde(rename = "bool")]
     Bool,
 }
@@ -208,26 +177,108 @@ pub enum LiteralKind {
 pub enum FunctionDefinitionKind {
     #[serde(rename = "function")]
     Function,
-
     #[serde(rename = "constructor")]
     Constructor,
-
     #[serde(rename = "fallback")]
     Fallback,
-
     #[serde(rename = "receive")]
     Receive,
-
     #[serde(rename = "modifier")]
     Modifier,
-
     #[serde(rename = "event")]
     Event,
-
     #[serde(rename = "freeFunction")]
     FreeFunction
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum NodeType {
+    // Expressions
+    Assignment,
+    BinaryOperation,
+    Conditional,
+    ElementaryTypeNameExpression,
+    FunctionCall,
+    FunctionCallOptions,
+    Identifier,
+    IndexAccess,
+    IndexRangeAccess,
+    Literal,
+    MemberAccess,
+    NewExpression,
+    TupleExpression,
+    UnaryOperation,
+
+    // Statements
+    Block,
+    Break,
+    Continue,
+    DoWhileStatement,
+    EmitStatement,
+    ExpressionStatement,
+    ForStatement,
+    IfStatement,
+    InlineAssembly,
+    PlaceholderStatement,
+    Return,
+    RevertStatement,
+    TryStatement,
+    UncheckedBlock,
+    VariableDeclarationStatement,
+    VariableDeclaration,
+    WhileStatement,
+
+    // Yul statements
+    YulAssignment,
+    YulBlock,
+    YulBreak,
+    YulContinue,
+    YulExpressionStatement,
+    YulLeave,
+    YulForLoop,
+    YulFunctionDefinition,
+    YulIf,
+    YulSwitch,
+    YulVariableDeclaration,
+
+    // Yul expressions
+    YulFunctionCall,
+    YulIdentifier,
+    YulLiteral,
+
+    // Yul literals
+    YulLiteralValue,
+    YulHexValue,
+
+    // Definitions
+    ContractDefinition,
+    FunctionDefinition,
+    EventDefinition,
+    ErrorDefinition,
+    ModifierDefinition,
+    StructDefinition,
+    EnumDefinition,
+    UserDefinedValueTypeDefinition,
+
+    // Directives
+    PragmaDirective,
+    ImportDirective,
+    UsingForDirective,
+
+    // Misc
+    SourceUnit,
+    InheritanceSpecifier,
+    ElementaryTypeName,
+    FunctionTypeName,
+    ParameterList,
+    TryCatchClause,
+    ModifierInvocation,
+
+    /// An unknown AST node type.
+    Other(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct NodeReference {
     pub id: usize,
     #[serde(rename = "nodeType")]
@@ -237,6 +288,7 @@ pub struct NodeReference {
     pub other: BTreeMap<String, serde_json::Value>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct SymbolAliases {
     foreign: NodeReference,
     local: Option<String>,
@@ -245,25 +297,11 @@ pub struct SymbolAliases {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct InheritanceSpecifier {
-    id: usize,
-    src: SourceLocation,
-    #[serde(rename = "arguments")]
-    arguments: Option<Vec<Expression>>,
-    #[serde(rename = "baseName")]
-    base_name: BaseName,
-    #[serde(rename = "nodeType")]
-    node_type: NodeType,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum ContractKind {
     #[serde(rename = "contract")]
     Contract,
-
     #[serde(rename = "interface")]
     Interface,
-
     #[serde(rename = "library")]
     Library
 }
@@ -291,7 +329,7 @@ pub struct Node {
     pub file: Option<String>,
     #[serde(rename = "canonicalName")]
     pub canonical_name: Option<String>,
-    pub members: Option<Vec<NodeReference>>,
+    pub members: Option<Vec<Node>>,
     pub scope: Option<usize>,
     #[serde(rename = "typeIdentifier")]
     pub type_identifier: Option<String>,
@@ -303,7 +341,7 @@ pub struct Node {
     #[serde(rename = "license")]
     pub license: Option<String>,
     #[serde(rename = "nodes")]
-    pub nodes: Option<Vec<NodeReference>>,
+    pub nodes: Option<Vec<Node>>,
     #[serde(rename = "sourceUnit")]
     pub source_unit: Option<usize>,
     #[serde(rename = "unitAlias")]
@@ -311,17 +349,17 @@ pub struct Node {
     #[serde(rename = "abstract")]
     pub is_abstract: Option<bool>,
     #[serde(rename = "baseContracts")]
-    pub base_contracts: Option<Vec<InheritanceSpecifier>>,
+    pub base_contracts: Option<Vec<Node>>,
     #[serde(rename = "functionList")]
-    pub function_list: Option<Vec<NodeReference>>,
-    pub function: Option<NodeReference>,
+    pub function_list: Option<Vec<Node>>,
+    pub function: Option<Box<Node>>,
     #[serde(rename = "contractDependencies")]
     pub contract_dependencies: Option<Vec<usize>>,
     #[serde(rename = "contractKind")]
     pub contract_kind: Option<ContractKind>,
     pub global: Option<bool>,
     #[serde(rename = "documentation")]
-    pub documentation: Option<StructuredDocumentation>,  // ou string
+    pub documentation: Option<Box<Node>>,  // ou string
     #[serde(rename = "underlyingType")]
     pub underlying_type: Option<String>,
     #[serde(rename = "fullyImplemented")]
@@ -331,54 +369,54 @@ pub struct Node {
     #[serde(rename = "usedErrors")]
     pub used_errors: Option<Vec<usize>>,
     #[serde(rename = "arguments")]
-    pub arguments: Option<Vec<NodeReference>>,  // ou juste NodeReference
+    pub arguments: Option<Vec<Node>>,  // ou juste NodeReference
     pub visibility: Option<Visibility>,
     #[serde(rename = "baseName")]
-    pub base_name: Option<NodeReference>,
+    pub base_name: Option<Box<Node>>,
     #[serde(rename = "baseModifiers")]
     pub base_modifiers: Option<Vec<usize>>,
-    pub overrides: Option<NodeReference>,
-    pub body : Option<Node>,
+    pub overrides: Option<Box<Node>>,
+    pub body : Option<Box<Node>>,
     #[serde(rename = "referencedDeclaration")]
     pub referenced_declaration: Option<usize>,
     #[serde(rename = "pathNode")]
-    pub path_node: Option<NodeReference>,
+    pub path_node: Option<Box<Node>>,
     #[serde(rename = "isVirtual")]
     pub is_virtual: Option<bool>,
-    pub parameters: Option<NodeReference>,
-    pub kind: Option<NodeReference>,
+    pub parameters: Option<Box<Node>>,
+    pub kind: Option<Box<Node>>,
     #[serde(rename = "modifierName")]
-    pub modifier_name: Option<NodeReference>,
-    pub condition: Option<NodeReference>,
-    pub statements: Option<Vec<NodeReference>>,
-    pub block: Option<NodeReference>,
+    pub modifier_name: Option<Box<Node>>,
+    pub condition: Option<Box<Node>>,
+    pub statements: Option<Vec<Node>>,
+    pub block: Option<Box<Node>>,
     #[serde(rename = "errorName")]
     pub error_name: Option<String>,
-    pub error_call: Option<NodeReference>,
-    pub external_call: Option<NodeReference>,
-    pub clauses: Option<Vec<NodeReference>>,
-    pub expression: Option<NodeReference>,
+    pub error_call: Option<Box<Node>>,
+    pub external_call: Option<Box<Node>>,
+    pub clauses: Option<Vec<Node>>,
+    pub expression: Option<Box<Node>>,
     #[serde(rename = "falseBody")]
-    pub false_body: Option<NodeReference>,
+    pub false_body: Option<Box<Node>>,
     #[serde(rename = "trueBody")]
-    pub true_body: IfStatementBody,
+    pub true_body: Option<Box<Node>>,
     pub function_return_parameters: Option<usize>,
     pub assignments: Option<Vec<usize>>,
-    pub declarations: Option<Vec<Option<VariableDeclaration>>>,
+    pub declarations: Option<Vec<Option<Box<Node>>>>,
     #[serde(rename = "initialValue")]
-    pub initial_value: Option<NodeReference>,
+    pub initial_value: Option<Box<Node>>,
     #[serde(rename = "initializationExpression")]
-    pub initialization_expression: Option<NodeReference>,
+    pub initialization_expression: Option<Box<Node>>,
     #[serde(rename = "loopExpression")]
-    pub loop_expression: Option<NodeReference>,
+    pub loop_expression: Option<Box<Node>>,
     #[serde(rename = "eventCall")]
-    pub event_call: Option<NodeReference>,
+    pub event_call: Option<Box<Node>>,
     #[serde(rename = "virtual")]
     pub id_virtual: Option<bool>,
     #[serde(rename = "stateMutability")]
     pub state_mutability: Option<StateMutability>,
     #[serde(rename = "returnParameters")]
-    pub return_parameters: Option<NodeReference>,
+    pub return_parameters: Option<Box<Node>>,
     #[serde(rename = "baseFunctions")]
     pub base_functions: Option<Vec<usize>>,
     #[serde(rename = "functionSelector")]
@@ -406,14 +444,14 @@ pub struct Node {
     pub operator: Option<UnaryOperator>,
     pub prefix: Option<bool>,
     #[serde(rename = "subExpression")]
-    pub sub_expression: Option<NodeReference>,
-    pub components: Option<Vec<NodeReference>>,
+    pub sub_expression: Option<Box<Node>>,
+    pub components: Option<Vec<Node>>,
     #[serde(rename = "isInlineArray")]
     pub is_inline_array: Option<bool>,
     #[serde(rename = "keyType")]
-    pub key_type: Option<NodeReference>,
+    pub key_type: Option<Box<Node>>,
     #[serde(rename = "valueType")]
-    pub value_type: Option<NodeReference>,
+    pub value_type: Option<Box<Node>>,
     pub indexed: Option<bool>,
     pub mutability: Option<Mutability>,
     #[serde(rename = "stateVariable")]
@@ -421,46 +459,46 @@ pub struct Node {
     #[serde(rename = "storageLocation")]
     pub storage_location: Option<StorageLocation>,
     #[serde(rename = "typeName")]
-    pub type_name: Option<NodeReference>,
+    pub type_name: Option<Box<Node>>,
     #[serde(rename = "value")]
-    pub value: Option<NodeReference>,
+    pub value: Option<Box<Node>>,
     #[serde(rename = "parameterTypes")]
-    pub parameter_types: Option<NodeReference>,
+    pub parameter_types: Option<Box<Node>>,
     #[serde(rename = "returnParameterTypes")]
-    pub return_parameter_types: Option<NodeReference>,
+    pub return_parameter_types: Option<Box<Node>>,
     #[serde(rename = "baseType")]
-    pub base_type: Option<NodeReference>,
-    pub length: Option<NodeReference>,
+    pub base_type: Option<Box<Node>>,
+    pub length: Option<Box<Node>>,
     #[serde(rename = "memberName")]
     pub member_name: Option<String>,
     #[serde(rename = "hexValue")]
     pub hex_value: Option<String>,
     #[serde(rename = "baseExpression")]
-    pub base_expression: NodeReference,
+    pub base_expression: Option<Box<Node>>,
     #[serde(rename = "endExpression", skip_serializing_if = "Option::is_none")]
-    pub end_expression: Option<NodeReference>,
+    pub end_expression: Option<Box<Node>>,
     #[serde(rename = "startExpression", skip_serializing_if = "Option::is_none")]
-    pub start_expression: Option<NodeReference>,
+    pub start_expression: Option<Box<Node>>,
     #[serde(rename = "indexExpression", skip_serializing_if = "Option::is_none")]
-    pub index_expression: Option<NodeReference>,
+    pub index_expression: Option<Box<Node>>,
     #[serde(rename = "overloadedDeclarations")]
     pub overloaded_declarations: Option<Vec<usize>>,
     pub names: Option<Vec<String>>,
-    pub options: Option<Vec<NodeReference>>,
+    pub options: Option<Vec<Node>>,
     #[serde(rename = "tryCall")]
     pub try_call: Option<bool>,
     #[serde(rename = "falseExpression")]
-    pub false_expression: Option<NodeReference>,
+    pub false_expression: Option<Box<Node>>,
     #[serde(rename = "trueExpression")]
-    pub true_expression: Option<NodeReference>,
+    pub true_expression: Option<Box<Node>>,
     #[serde(rename = "commonType")]
     pub common_type: Option<TypeDescriptions>,
     #[serde(rename = "leftExpression")]
-    pub left_expression: Option<NodeReference>,
+    pub left_expression: Option<Box<Node>>,
     #[serde(rename = "rightExpression")]
-    pub right_expression: Option<NodeReference>,
+    pub right_expression: Option<Box<Node>>,
     #[serde(rename = "leftHandSide")]
-    pub left_hand_side: Option<NodeReference>,
+    pub left_hand_side: Option<Box<Node>>,
     #[serde(rename = "rightHandSide")]
-    pub right_hand_side: Option<NodeReference>,
+    pub right_hand_side: Option<Box<Node>>,
 }
